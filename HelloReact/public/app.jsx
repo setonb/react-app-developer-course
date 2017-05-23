4a6203f591a85350c73980396194b91c48b1b285
Seton Brown <seton_brown@ziffdavis.com>
2017-05-23 14:41:03 -0400

    Adding basic action for a form

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -6,6 +6,13 @@ var Greeter = React.createClass({
       message: 'This is from the component.'
     }
   },
+  onButtonClick: function(e) {
+    e.preventDefault();
+    // Getting the value from the "refs" in the action
+    var name = this.refs.name.value;
+    
+    alert(name);
+  },
   render: function() {
     var name = this.props.name;
     var message = this.props.message;
@@ -13,7 +20,12 @@ var Greeter = React.createClass({
     return (
       <div>
         <h1>Hello {name}</h1>
-      <p>{message}</p>
+        <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
