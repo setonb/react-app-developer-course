16efe411b9e2dbfda6a17f3f935888b99da5bb30
Seton Brown <seton_brown@ziffdavis.com>
2017-05-23 14:34:11 -0400

    Adding prop values and their defaults

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,15 +1,27 @@
 var Greeter = React.createClass({
+  // set default values for properties passed in
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is from the component.'
+    }
+  },
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Hello React</h1>
-        <p>This is from the component.</p>
+        <h1>Hello {name}</h1>
+      <p>{message}</p>
       </div>
     );
   }
 });
 
+var firstName = "Seton"
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message="This is from the element!"/>,
   document.getElementById('app')
 );
