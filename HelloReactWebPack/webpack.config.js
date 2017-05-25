e5d33ec56fbe3a020ec6166a563fac5c5862c1b3
Seton Brown <seton_brown@ziffdavis.com>
2017-05-25 16:11:05 -0400

    Adding Aliases in WebPack to allow the use of simple imports

--- HelloReactWebPack/webpack.config.js
+++ HelloReactWebPack/webpack.config.js
@@ -5,6 +5,12 @@ module.exports = {
     filename: './public/bundle.js'
   },
   resolve: {
+    root: __dirname, // available in node.js ( no funny business )
+    alias: {  // Allows us to just reference the name instead of the directory
+      Greeter: 'public/components/Greeter.jsx',
+      GreeterMessage: 'public/components/GreeterMessage.jsx',
+      GreeterForm: 'public/components/GreeterForm.jsx'
+    },
     extensions: ['', '.js', '.jsx']
   },
   module: {
